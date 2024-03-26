a439d2c53c5946eb79ec08622b2809986ef52690
Rumeysa <michiru.aware.lrn@gmail.com>
2024-03-26 22:28:29 +0300

    Create DetailsViewController for second VC

---
+++ ShoppingListWithCoreData/DetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailsViewController.swift
+//  ShoppingListWithCoreData
+//
+//  Created by Aydın Kutlu on 26.03.2024.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
