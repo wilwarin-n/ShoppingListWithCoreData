d66dfe91806e51a4d7f45ae7ac7f4b5f775b73ca
Rumeysa <michiru.aware.lrn@gmail.com>
2024-03-27 15:54:09 +0300

    created add image func
    with gestureRecognizer and pickercontroller

--- ShoppingListWithCoreData/DetailsViewController.swift
+++ ShoppingListWithCoreData/DetailsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var itemSize: UITextField!
     @IBOutlet weak var itemPrice:UITextField!
@@ -18,8 +18,22 @@ class DetailsViewController: UIViewController {
         //Klavye kapatmak için..
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
+        
+        let imageGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        view.addGestureRecognizer(imageGestureRecognizer)
     }
     
+    @objc func selectImage(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        image.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
     @IBAction func saveClicked(_ sender: Any) {
     }
     
